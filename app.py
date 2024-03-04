9c8b8628dd4a4175c31ec7c48a59e0f772420fe4
এ許我辞忧࿐♡ <127636623+Smiling-Weeping-zhr@users.noreply.github.com>
2024-03-04 19:13:51 +0800

    Update app.py
    update the file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("lmdeploy serve api_server ./workspace --server-name 0.0.0.0 --server-port 7860 --tp 1")
+os.system("lmdeploy serve api_server workspace --server-name 0.0.0.0 --server-port 7860 --tp 1")
 os.system("lmdeploy serve api_client http://localhost:23333")
