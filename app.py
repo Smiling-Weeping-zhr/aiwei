73958b3cad7988d2d015ef648c433538d1f65efd
এ許我辞忧࿐♡ <127636623+Smiling-Weeping-zhr@users.noreply.github.com>
2024-03-06 19:01:48 +0800

    Update app.py
    update

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
 import os
 
-os.system("lmdeploy serve api_server workspace --server-name 0.0.0.0 --server-port 7860 --tp 1")
-os.system("lmdeploy serve api_client http://localhost:23333")
+os.system('python api.py')
